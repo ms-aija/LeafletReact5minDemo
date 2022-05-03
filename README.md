212890afeab255bb9542e22dad98c981e80617b7
ms-aija <aija.avota@gmail.com>
2022-05-03 13:06:09 +0200

    updated readme and added comments to code to refer to action items from the readme

--- README.md
+++ README.md
@@ -2,15 +2,21 @@
 
 *See example in Map.js component. All CSS styles are found in index.css.*
 
-1. Install `react-leaflet` and `leaflet`
-2. Import `MapContainer`, `TileLayer`, `Marker`, `Popup` from `react-leaflet` and  `Icon` from `leaflet`
-3. Add leaflet stylesheet and scripts to `index.html` `<head>` (see https://leafletjs.com/examples/quick-start/)
-4. Add `.leaflet-container` class to css with map dimensions
-5. Add `MapContainer` component inside a `div` with id="map" (see https://react-leaflet.js.org/)
+1. Install `react-leaflet` and `leaflet` (*see code in Map.js component*)
+
+2. Import `MapContainer`, `TileLayer`, `Marker`, `Popup` from `react-leaflet` and  `Icon` from `leaflet` (*see code in Map.js component*)
+
+3. Add leaflet stylesheet and scripts to `index.html` `<head>` (*see code in index.html* and [leaflet.js docs](https://leafletjs.com/examples/quick-start/))
+
+4. Add `.leaflet-container` class to css with map dimensions. Note that the class **does not** have to added anywhere in the map component itself. (*see code in index.css*)
+
+5. Add `MapContainer` component inside a `div` with id="map" (*see code in Map.js component* and [react leaflet docs](https://react-leaflet.js.org/))
 
 --OPTIONAL--
 
-6. Create a custom marker by defining new instance of `Ico`n (see customIcon variable in the example) and add `icon` property to `Marker` component
-7. Add custom tiles by updating `attribution` and `url` properties in `TileLayer` component (see some options at https://leaflet-extras.github.io/leaflet-providers/preview/) (and watch The Billion Dollar Code https://www.imdb.com/title/tt15392100/ )
+6. Create a custom marker by defining new instance of `Icon` (see customIcon variable in the example) and add `icon` property to `Marker` component (*see code in Map.js component*)
+
+7. Add custom tiles by updating `attribution` and `url` properties in `TileLayer` component (*see code in Map.js component*, and some tile options from [leaflet extras](https://leaflet-extras.github.io/leaflet-providers/preview/))
+(and watch [The Billion Dollar Code](https://www.imdb.com/title/tt15392100/) to learn about tiles with some entertainment value)
 
 
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,41 +1,43 @@
-// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-// import { Icon } from 'leaflet';
-
+// --- (1) & (2): install and import ---
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+// --- ----------------------------- ---
 
 export function Map() {
-
-  // const customIcon = new Icon({
-  //   iconUrl: '/icons8-select-24.png',
-  //   iconSize: [33, 33],
-  //   // iconAnchor: [1, 1],
-  //   // popupAnchor: [-0, -76]
-  // })
-
   // Berlin coordinates
-  // const position = [52.51, 13.38]
+  const position = [52.51, 13.38]
+
+  // --- (6) Create a custom marker ---
+  const customIcon = new Icon({
+    iconUrl: '/icons8-select-24.png',
+    iconSize: [33, 33],
+    // iconAnchor: [1, 1],
+    // popupAnchor: [-0, -76]
+  })
 
   return (
     <section className='map-component' >
       <div className='map' id='map' >
-        <p>Show me a map here</p>
 
-      {/* --- Leaflet map */}
-      {/* <MapContainer center={position} zoom={6} scrollWheelZoom={true}>
+      {/* --- (5) Add leaflet map container --- */}
+      <MapContainer center={position} zoom={6} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          // --- Alternative map style
+          // --- (7) Alternative map style (attribution and url copied from the leaflet extras website) ---
           // attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
           // url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
+          // --- -------------------------------------------------------------------------------------- ---
         />
         <Marker position={position}
-          // icon={customIcon}
+          icon={customIcon}
         >
           <Popup>
             🐻🍻🎉
           </Popup>
         </Marker>
-      </MapContainer> */}
+      </MapContainer>
+      {/* --- ---------------------------- --- */}
       </div>
     </section>
   )
